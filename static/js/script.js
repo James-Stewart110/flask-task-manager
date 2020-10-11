e0a291e76bc85f109f72f8765e83f3b9e4fa4b20
James-Stewart110 <jamesstewart506@hotmail.co.uk>
2020-10-11 15:15:17 +0000

    Add task collasible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,9 @@
+// Navbar
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: 'right'});
+  });
+
+// Accordion
+$(document).ready(function(){
+    $('.collapsible').collapsible();
   });
